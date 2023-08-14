9b4f9c6a40c66c85a6c5685dc4f9bec0aa3ad637
PabloR18 <115594757+PabloR18@users.noreply.github.com>
2023-08-14 19:43:19 -0300

    ....

--- CambioMoedas.java
+++ CambioMoedas.java
@@ -2,22 +2,22 @@ package entities;
 
 public class CambioMoedas {
     
-    public static final double dolar = 4.91;
-    public static final double euro = 5.38;
-    public static final double yuan = 0.68;
+    public static final double DOLAR = 4.91;
+    public static final double EURO = 5.38;
+    public static final double YUAN = 0.68;
     
     public double quantidadeInformada;
     
     public double calculoDoDolar(double quantidadeInformada) {
-   	 return dolar * quantidadeInformada;
+   	 return DOLAR * quantidadeInformada;
     }
     
     public double calculoDoEuro(double quantidadeInformada) {
-   	 return euro * quantidadeInformada;
+   	 return EURO * quantidadeInformada;
     }
     
     public double calculoDoYuan(double quantidadeInformada) {
-   	 return quantidadeInformada * yuan;
+   	 return quantidadeInformada * YUAN;
     }
     
     //Desenvolvedor: Pablo Rodrigues
